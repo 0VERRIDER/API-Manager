5f202d6a651ab5dbd28f528ab766f8444b9e309c
Anshil <contactme@anshil.me>
2023-12-23 04:02:21 +0000

    Add UsersModule and TypeOrmModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { configService } from './config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, 
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
