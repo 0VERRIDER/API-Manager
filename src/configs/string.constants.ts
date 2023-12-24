261486764786c5b70e918a0b3f51d34cdf6fd585
Anshil <contactme@anshil.me>
2023-12-24 17:19:04 +0000

    Update USER_STRINGS to include USERS_FOUND

--- src/configs/string.constants.ts
+++ src/configs/string.constants.ts
@@ -1,5 +1,6 @@
 export const USER_STRINGS = {
     USER_FOUND: 'User found',
+    USERS_FOUND: 'Users found',
     USER_FOUND_ID: (id: string) => `User with id ${id} found`,
     NO_USERS_FOUND: 'No users found',
     NO_USER_ID_FOUND: (id: string) => `No user with id ${id} found`,
