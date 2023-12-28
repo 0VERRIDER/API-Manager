b20656128c49f568755b64318075930ecce25be9
Anshil <contactme@anshil.me>
2023-12-28 05:27:06 +0000

    Handle Redis connection and operation errors

--- src/data/redis/redis.service.ts
+++ src/data/redis/redis.service.ts
@@ -7,18 +7,38 @@ export class RedisService {
     private readonly client: Redis;
 
     constructor() {
-        this.client = new Redis(redisConfig);
+        try {
+            this.client = new Redis(redisConfig);
+        } catch (error) {
+            console.error('Error connecting to Redis:', error);
+            throw error;
+        }
     }
 
     async get(key: string): Promise<string | null> {
-        return this.client.get(key);
+        try {
+            return await this.client.get(key);
+        } catch (error) {
+            console.error('Error getting value from Redis:', error);
+            throw error;
+        }
     }
 
     async set(key: string, value: string): Promise<void> {
-        await this.client.set(key, value);
+        try {
+            await this.client.set(key, value);
+        } catch (error) {
+            console.error('Error setting value in Redis:', error);
+            throw error;
+        }
     }
 
     async del(key: string): Promise<void> {
-        await this.client.del(key);
+        try {
+            await this.client.del(key);
+        } catch (error) {
+            console.error('Error deleting value from Redis:', error);
+            throw error;
+        }
     }
 }
